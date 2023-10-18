b8747f3876af4cc3ba3b1348a761cee4694a4600
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-18 08:10:12 +0100

    PRINTF ALX

--- converter.c
+++ converter.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * convert - converts number and base into string
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
+ * convert - program that convert num to str
+ * @num: number parsed
+ * @base: parsed variables
+ * @lowercase: Lowercase flag
+ * Return: string resulted
  */
+
 char *convert(unsigned long int num, int base, int lowercase)
 {
 	static char *rep;
@@ -16,8 +17,10 @@ char *convert(unsigned long int num, int base, int lowercase)
 	rep = (lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
+
 	ptr = &buffer[49];
 	*ptr = '\0';
+
 	do {
 		*--ptr = rep[num % base];
 		num /= base;
